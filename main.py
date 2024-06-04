feab4908ffc908cb1529d7bfa2b285956bea9ff5
Anton Duchenko <deragandi0@gmail.com>
2024-06-04 19:34:34 +0300

    add cors

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import router as todo_router
 from database import engine
 import models
 
 app = FastAPI()
 app.include_router(todo_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 models.Base.metadata.create_all(bind=engine)
